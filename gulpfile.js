5011eb61d2052a5185e2210296480a4deb41dce6
Danil Savchenko <danil.savchenko.dev@gmail.com>
2021-08-30 16:27:47 +0600

    Add gulp setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 const { src, dest, series, parallel, watch } = require('gulp');
-const less = require('gulp-less');
-const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-
+const sass = require('gulp-sass')(require('sass'));
 
 function pugPages() {
   return src('./src/views/pages/*.pug')
@@ -13,58 +10,47 @@ function pugPages() {
     .on('end', browserSync.reload);
 }
 
+function pugBlog() {
+  return src('./src/views/blog/*.pug')
+    .pipe(pug({ pretty: true }))
+    .pipe(dest('./blog'))
+    .on('end', browserSync.reload);
+}
+
+function pugPortfolio() {
+  return src('./src/views/portfolio/*.pug')
+    .pipe(pug({ pretty: true }))
+    .pipe(dest('./portfolio'))
+    .on('end', browserSync.reload);
+}
+
 function styles() {
-  return src('./src/static/styles/style.scss')
+  return src('./src/styles/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./dist/css'))
     .pipe(browserSync.stream());
 }
 
-function images() {
-  return src('./src/static/img/**/*')
-    .pipe(dest('./build/img'));
-}
-
-function fonts() {
-  return src('./src/static/fonts/**/*')
-    .pipe(dest('./build/fonts'));
-}
-
-function cleanBuild() {
-  return src('./build', { read: false, allowEmpty: true }).pipe(clean())
-}
-
 function serve(cb) {
   browserSync.init({
-    server: "./build",
+    server: "./",
     notify: false,
     scrollProportionally: false,
   });
 }
 
 function watchFiles() {
-  // html nunjucks
-  watch('./src/html/**/*.njk', series(html));
   // pug
-  watch('./src/pug/**/*.pug', series(htmlPug));
+  watch('./src/views/**/*.pug', series(pugPages, pugBlog, pugPortfolio));
   // styles less
   watch('./src/static/styles/**/*.less', series(styles));
-  // styles sass
-  watch('./src/static/styles/**/*.scss', series(stylesSass));
-  // images
-  watch('./src/static/img/**/*', series(images));
-  // fonts
-  watch('./src/static/fonts/**/*', series(fonts));
 }
 
-exports.html = html
-exports.htmlPug = htmlPug
+exports.pugPages = pugPages
+exports.pugBlog = pugBlog
+exports.pugPortfolio = pugPortfolio
 exports.styles = styles
-exports.stylesSass = stylesSass
-exports.images = images
-exports.fonts = fonts
-exports.cleanBuild = cleanBuild
 exports.serve = serve
 exports.watchFiles = watchFiles
 
-exports.default = series(cleanBuild, parallel(htmlPug, stylesSass, images, fonts), parallel(watchFiles, serve));
+exports.default = series(parallel(pugPages, pugBlog, pugPortfolio, styles), parallel(watchFiles, serve));
